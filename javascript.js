3751557b7eb6dec58cfd6b9e9c856e1cad3f66a3
MaricelGentile <marigen10arg@gmail.com>
2022-11-27 23:25:04 -0300

    esto es un comentario

---
+++ javascript.js
@@ -0,0 +1,17 @@
+alert("Hello World!");
+//VARIABLES
+// Una variable es un contenedor de información
+ 
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente = pais + " " + continente;
+
+pais = "México";
+continente = "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);
+
+
+
